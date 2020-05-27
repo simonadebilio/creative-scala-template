dd6c4e14702b3f80da60f908bdcf8758315e9349
Simona Debilio <simona.debilio@gmail.com>
2020-05-27 18:02:10 +0100

    Add MyExample

---
+++ src/main/scala/MyExample.scala
@@ -0,0 +1,9 @@
+import doodle.core._
+import doodle.image._
+import doodle.image.syntax._
+import doodle.image.syntax.core._
+import doodle.java2d._
+
+object MyExample {
+    Image.circle(400).fillColor(Color.white).under(Image.circle(300).fillColor(Color.black)).under(Image.circle(200).fillColor(Color.blue)).under(Image.circle(100).fillColor(Color.red)).under(Image.circle(50).fillColor(Color.yellow)).above(Image.rectangle(50,100).fillColor(Color.brown)).above(Image.rectangle(150,50).fillColor(Color.brown)).above(Image.rectangle(400,100).fillColor(Color.green)).draw()
+}
