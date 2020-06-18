bc56727442f9d444146258768906725018cecd4a
Simona Debilio <simona.debilio@gmail.com>
2020-06-18 17:23:26 +0100

    Add exercise chapter 9 (Parametric Spiral)

---
+++ src/main/scala/chapter09/ParametricSpiral.scala
@@ -0,0 +1,49 @@
+package chapter09
+
+import doodle.core._
+import doodle.image._
+import doodle.image.syntax.core._
+
+object ParametricSpiral {
+  def sample(samples: Int, image: Image, curve: Angle => Point): Image = {
+    val step = Angle.one / samples
+
+    def loop(count: Int): Image = {
+      val angle = step * count
+      count match {
+        case 0 => Image.empty
+        case n =>
+          image.at(curve(angle)).on(loop(n - 1))
+      }
+    }
+
+    loop(samples)
+  }
+
+  def spiral(angle: Angle, ratio: Double, rotation: Double): Point =
+    Point.polar((angle * ratio).toDegrees, angle + rotation.degrees)
+
+  def parametricSpiral(samples: Int, ratio: Double, rotation: Double): Image = {
+    val dot = Image
+      .triangle(10, 10)
+      .fillColor(Color.limeGreen)
+      .strokeColor(Color.lawngreen)
+
+    sample(samples, dot, spiral(_, ratio, rotation))
+  }
+
+  def allSpirals(numberOfSpirals: Int): Image = {
+    val step = Angle.one / numberOfSpirals
+
+    def loop(count: Int): Image = {
+      val angle = step * count
+      count match {
+        case 0 => Image.empty
+        case n =>
+          parametricSpiral(samples = 200, ratio = 0.5, rotation = angle.toDegrees).on(loop(n - 1))
+      }
+    }
+
+    loop(numberOfSpirals)
+  }
+}
